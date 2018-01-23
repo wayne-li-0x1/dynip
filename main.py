75cf538d85761f25c9c9ba7b393758f6dff4913c
Wayne Li <wayne.li.0x1@gmail.com>
2018-01-23 02:38:10 -0800

    add route to show env in plain text

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from bottle import route, run
 from bottle import request
 
-@route('/hello')
-def hello():
+@route('/get_my_ip')
+def get_my_ip():
     return request.environ.get("REMOTE_ADDR")
+
+@route('/get_my_ip.json')
+def get_my_ip_json():
+    clientip = request.environ.get("REMOTE_ADDR")
+    return {"ip":clientip}
+
+@route('/env')
+def get_env():
+    import json
+    d = {}	
+    ks = list(request.environ)
+    s = ""
+    ks.sort()
+    for k in ks:
+        s = s + "%s : %s\n"%(k, request.environ[k])
+    from bottle import response
+    response.content_type='text/plain'
+    return s
+     
+     
 
 
 run(host='0.0.0.0', port=8080, debug=True)
